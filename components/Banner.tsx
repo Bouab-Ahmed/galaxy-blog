f9db401e93b2619b10d5114a92d7b291691d6b3b
BOUAB Ahmed <bouab.ahmed757@gmail.com>
2023-02-27 10:01:17 +0100

    add banner

---
+++ components/Banner.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+function Banner() {
+  return (
+    <div className=' md:mt-3 mt-0 flex flex-col lg:flex-row lg:items-center lg:justify-between lg:space-x-5 font-bold py-5 px-10 mb-10'>
+      <div>
+        {/* eslint-disable-next-line react/no-unescaped-entities */}
+        <h1 className='text-7xl'>Galaxy's Daily Blog</h1>
+        <h2 className='mt-5 md:mt-0'>
+          Welcome to{' '}
+          <span className='underline decoration-4 decoration-[#F7AB0A]'>
+            Every Developers
+          </span>{' '}
+          favorite blog in the galaxy
+        </h2>
+      </div>
+
+      <p className='mt-5 md:mt-2 text-gray-400 max-w-sm'>
+        New product features | The latest in technology | The weakly debbuging
+        nightmares and more !
+      </p>
+    </div>
+  );
+}
+
+export default Banner;
