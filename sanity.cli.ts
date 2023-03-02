d061792475647d4a078b8d6b8028928a69dea0ef
BOUAB Ahmed <bouab.ahmed757@gmail.com>
2023-03-02 09:07:31 +0100

    updates

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -1,7 +1,7 @@
 import { defineCliConfig } from 'sanity/cli';
 
-const projectId = process.env.SANITY_PROJECT_ID!;
-const dataset = process.env.SANITY_DATASET!;
+// const projectId = process.env.SANITY_PROJECT_ID!;
+// const dataset = process.env.SANITY_DATASET!;
 
 export default defineCliConfig({
   api: {
